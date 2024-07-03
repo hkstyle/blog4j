ca7edb7a7bc7e57db3b86ca342b747c4daf37057
98k <2915524550@qq.com>
2024-07-03 12:34:52 +0800

    edit:完善根据用户ID获取角色信息的接口

--- blog4j-oss/src/main/java/com/blog4j/oss/OssApplication.java
+++ blog4j-oss/src/main/java/com/blog4j/oss/OssApplication.java
@@ -3,6 +3,7 @@ package com.blog4j.oss;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author 98k灬
@@ -12,6 +13,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  **/
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.blog4j.oss.feign")
 public class OssApplication {
     public static void main(String[] args) {
         SpringApplication.run(OssApplication.class, args);
--- blog4j-user/src/main/java/com/blog4j/user/service/impl/RoleServiceImpl.java
+++ blog4j-user/src/main/java/com/blog4j/user/service/impl/RoleServiceImpl.java
@@ -1,10 +1,19 @@
 package com.blog4j.user.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.blog4j.common.enums.ErrorEnum;
+import com.blog4j.common.exception.Blog4jException;
+import com.blog4j.common.vo.RoleInfoVo;
 import com.blog4j.user.entity.RoleEntity;
+import com.blog4j.user.entity.UserEntity;
 import com.blog4j.user.mapper.RoleMapper;
+import com.blog4j.user.mapper.UserMapper;
 import com.blog4j.user.service.RoleService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 /**
  * @author 98k灬
@@ -14,5 +23,29 @@ import org.springframework.stereotype.Service;
  **/
 @Service
 public class RoleServiceImpl extends ServiceImpl<RoleMapper, RoleEntity> implements RoleService {
+    @Autowired
+    private UserMapper userMapper;
 
+    /**
+     * 根据用户ID获取角色信息
+     *
+     * @param userId 用户ID
+     * @return 角色信息
+     */
+    @Override
+    public RoleInfoVo getRoleInfoByUserId(String userId) {
+        UserEntity user = userMapper.selectById(userId);
+        if (Objects.isNull(user)) {
+            throw new Blog4jException(ErrorEnum.USER_NOT_EXIST_ERROR);
+        }
+        RoleEntity roleEntity = this.baseMapper.selectById(user.getRoleId());
+        if (Objects.isNull(roleEntity)) {
+            log.error("Role info is null .");
+            throw new Blog4jException(ErrorEnum.ROLE_INFO_EMPTY_ERROR);
+        }
+
+        RoleInfoVo roleInfoVo = new RoleInfoVo();
+        BeanUtils.copyProperties(roleEntity, roleInfoVo);
+        return roleInfoVo;
+    }
 }
